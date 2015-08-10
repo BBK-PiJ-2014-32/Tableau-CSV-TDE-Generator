ab6c022ff7c0630f2adfac234c6d36ba90588d80
philhannant <philhannant@192.168.0.5>
2015-08-10 21:35:54 +0100

    playing with swing buttons

--- src/extracter/UserInterface.java
+++ src/extracter/UserInterface.java
@@ -7,10 +7,19 @@ import javax.swing.*;
 
 public class UserInterface {
 
+	public static JButton enterButton;
+	private static String ENTER = "Enter";
+    public static JTextArea output;
+    public static JTextField input;
+    static JFrame frame;
+    static JPanel panel;
+	public static String myString;
+	public static JTextArea editTextArea = new JTextArea("Type Here!");
+
 	
 	public static void setupFrame(){
 		JFrame jFrame = new JFrame("CSV TDE creator");
-		jFrame.setPreferredSize(new Dimension(250, 250));
+		jFrame.setPreferredSize(new Dimension(1000, 1000));
 		
 		JMenuBar greenMenuBar = new JMenuBar();
 		
@@ -18,10 +27,36 @@ public class UserInterface {
 		greenMenuBar.setBackground(new Color(154, 165, 127));
 		greenMenuBar.setPreferredSize(new Dimension(200, 20));
 		
+		JTextField textField = new JTextField();
+		textField.setBounds(6, 41, 134, 28);
+		jFrame.add(textField);
+	    textField.setColumns(10);
+
+	    enterButton = new JButton();
+        enterButton.setPreferredSize(new Dimension(40, 40));
+        jFrame.add(enterButton);
+     
+        JButton fat = new JButton("Fat");
+        JButton skinny = new JButton("Skinny");
+
+        JPanel buttonPane = new JPanel();
+        buttonPane.add(skinny);
+
+        JFrame frame = new JFrame("Testing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(fat, BorderLayout.NORTH);
+        frame.add(buttonPane, BorderLayout.SOUTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+		
 		jFrame.pack();
 		jFrame.setVisible(true);
-	}
+	
 
+	
+	}
 	public static void main(String[] args){
 		setupFrame();
 	}
